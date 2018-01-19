de279008465d83afcb3d0b6a690f6b83feb3db34
Codrut Covaci <covacic@gmail.com>
2018-01-19 16:27:28 +0200

    Add some files

--- src/main/java/org/codrutz/crypto/CryptoApplication.java
+++ src/main/java/org/codrutz/crypto/CryptoApplication.java
@@ -2,10 +2,17 @@ package org.codrutz.crypto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CryptoApplication {
+public class CryptoApplication extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CryptoApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoApplication.class, args);
 	}
--- src/main/java/org/codrutz/crypto/controller/LoginController.java
+++ src/main/java/org/codrutz/crypto/controller/LoginController.java
@@ -1,20 +1,21 @@
 package org.codrutz.crypto.controller;
 
-import java.lang.reflect.Method;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+
+@Controller
 public class LoginController {
 	
-	@RequestMapping(method=RequestMethod.GET,value="/login")
-	public String login() {
+	@RequestMapping("/")
+	public String login(Model model,@RequestParam(value="name",required=false,defaultValue="It works") String name) {
+		System.out.println("Mucu");
+		model.addAttribute("name",name);
 		return "login";
 	}
 
